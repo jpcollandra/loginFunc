adf76f51d8da648c225aab5e237a12ca8a966994
John Collandra <jpcollandra@gmail.com>
2022-02-17 22:59:19 -0500

    updated styling and react native elements

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeProvider } from "react-native-elements";
 import LoginPage from "./screens/loginPage";
 
 export default function App() {
   return (
     <View>
       <SafeAreaView>
-        <LoginPage/>
+        <ThemeProvider>
+          <LoginPage/>
+        </ThemeProvider>
       </SafeAreaView>
     </View>
   );
--- screens/loginPage.tsx
+++ screens/loginPage.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, StyleSheet, Dimensions } from "react-native";
-import { Input } from "react-native-elements";
-import { Button,Text } from "react-native-elements";
+import { TextInput } from "react-native";
+import { Button, Text } from "react-native-elements";
 
 export default function LoginPage() {
+  /*const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+
+
+  const loginPayload = {
+    username: username,
+    password: password,
+  };
+
+  const response = await fetch(""),{ 
+    method: "POST",
+    body: JSON.stringify(loginPayload),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+
+   } */
+
   return (
     <View>
       <View style={styles.container}>
@@ -12,14 +31,29 @@ export default function LoginPage() {
           source={require("../assets/bubblelogo.png")}
         />
       </View>
-      <View >
-        <Input style={styles.inputstyle} placeholder="Enter Username"/>
-        <Input style={styles.inputstyle} placeholder="Enter Password"/>
-       
-        <Text style={styles.textstyle}>Forgot Password?</Text>
-        <Button style={styles.buttonstyle} title="Login"/>
-        <Text style={styles.textstyle2}>Not a user yet? Register Now</Text>
+      <View style={styles.container2}>
+        <TextInput style={styles.inputstyle} placeholder="Enter Username" />
+        <TextInput style={styles.inputstyle} placeholder="Enter Password" />
 
+        <Text style={styles.textstyle}>Forgot Password?</Text>
+
+        <Button
+          title="Login"
+          buttonStyle={{
+            backgroundColor: "#00a680",
+            borderWidth: 2,
+            borderColor: "#00a680",
+            borderRadius: 30,
+          }}
+          containerStyle={{
+            width: 100,
+            marginHorizontal: "60%",
+            marginVertical: 10,
+          }}
+          titleStyle={{ fontSize: 12 }}
+        />
+
+        <Text style={styles.textstyle2}>Not a user yet? Register Now</Text>
       </View>
     </View>
   );
@@ -29,36 +63,37 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: "10%",
   },
+  container2: {
+    paddingTop: "30%",
+  },
   stretch: {
-    width: Dimensions.get("window").width/2,
-    height: Dimensions.get("window").height/4,
+    width: Dimensions.get("window").width / 2,
+    height: Dimensions.get("window").height / 4,
     resizeMode: "contain",
-    alignSelf: "center"
-  },
-  inputstyle:{
-    width: 50,
-    height: 50,
     alignSelf: "center",
-    },
-  buttonstyle:{
-    width: 100,
-    height: 50,
-    borderWidth: 2,
-    borderColor: 'white',
-    borderRadius: 100,
-    fontWeight: 'bold',
-    marginHorizontal: "60%",
+    paddingBottom: "10%",
   },
-    textstyle:{
-    fontSize: 14,
-    color: 'black',
+  inputstyle: {
+    width: Dimensions.get("window").width / 1.3,
+    height: "10%",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
     marginHorizontal: "10%",
-    },
-    textstyle2:{
-        fontSize: 14,
-        color: 'black',
-        marginHorizontal: "10%",
-        marginTop: "20%",
-        alignSelf: "center"
-        }
+    marginTop: "5%",
+  },
+  textstyle: {
+    fontSize: 14,
+    color: "black",
+    marginHorizontal: "10%",
+    marginTop: "5%",
+  },
+  textstyle2: {
+    fontSize: 14,
+    color: "black",
+    marginHorizontal: "10%",
+    marginTop: "20%",
+    alignSelf: "center",
+  },
 });
